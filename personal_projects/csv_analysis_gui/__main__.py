45e668c5220579f027af182c039c69226ed531c8
Ian Bradley Rust <ian.bradley.rust@walmart.com>
2023-01-22 17:22:04 -0600

    mapview

--- personal_projects/csv_analysis_gui/__main__.py
+++ personal_projects/csv_analysis_gui/__main__.py
@@ -1,25 +1,29 @@
 from .coordinators import MainCoordinator, MainCoordinatorResult
 from tkinter import *
 import sys
+import tkintermapview
 
 def main():
     mainWindow: Tk = Tk()
     mainWindow.title("GIS Dataset Analyzer")
     mainWindow.resizable(False, False)
-    mainWindow.geometry("900x700")
+    mainWindow.geometry(f"{800}x{600}")
 
-    coordinator = MainCoordinator(mainWindow)
+    # coordinator = MainCoordinator(mainWindow)
+
+    map_widget = tkintermapview.TkinterMapView(mainWindow, width=800, height=600, corner_radius=0)
+    map_widget.place(relx=0.5, rely=0.5, anchor=CENTER)
 
     def callback(result: MainCoordinatorResult):
         if result != MainCoordinatorResult.SUCCESS:
             print(result, result.value)
             sys.exit(f"Error - Coordinator Exited with {result}")
 
-    coordinator.onFinishCallback = callback
-    coordinator.start()
+    # coordinator.onFinishCallback = callback
+    # coordinator.start()
 
     mainWindow.mainloop()                       # blocks until the user closes the window
 
-    coordinator.finish(MainCoordinatorResult.SUCCESS)
+    # coordinator.finish(MainCoordinatorResult.SUCCESS)
 
 main()
