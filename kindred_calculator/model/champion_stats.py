9dbe6e1dfb39b8e053da6a0f452c5d2cb17ddf9c
ian rust <ibrust777@gmail.com>
2023-10-17 11:50:44 -0500

    misc updates

---
+++ kindred_calculator/model/champion_stats.py
@@ -0,0 +1,12 @@
+
+class ChampionStats:
+    attackDamage: float
+    attackSpeed: float
+    criticalStrike: float
+    abilityHaste: float
+    abilityPower: float
+    armorPen: float
+    magicPen: float
+
+    level: int
+
