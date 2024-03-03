a1c27f10b75c53f6c82831b7f5d6c8856b1deb3d
ian rust <ibrust777@gmail.com>
2024-03-03 12:21:13 -0600

    misc

--- personal_projects/music_generator/test.kt
+++ personal_projects/music_generator/test.kt
@@ -2,7 +2,10 @@ import kotlin.math.abs
 import kotlin.math.floor
 
 fun main() {
-    println()
+    // TODO: initialize board and make some moves
+    // print out the board
+    val board = ChessBoard()
+    println(board)
 }
 
 class ChessBoard() {
@@ -11,6 +14,19 @@ class ChessBoard() {
 
     init {
         setupBoard()
+    }
+
+    override fun toString(): String {
+        var string = ""
+        for (row in Row.values()) {
+            string += "\n"
+            for (column in Column.values()) {
+                val index = getSquaresIndex(row, column)
+                val emptySquare = "- "
+                string += "${board[index] ?: emptySquare}"
+            }
+        }
+        return string
     }
 
     fun setupBoard() {
@@ -30,15 +46,15 @@ class ChessBoard() {
         board[getSquaresIndex(Row.EIGHT, Column.A)] = Rook(color = ChessColor.BLACK)
         board[getSquaresIndex(Row.EIGHT, Column.H)] = Rook(color = ChessColor.BLACK)
 
-        board[getSquaresIndex(Row.ONE, Column.B)] = Bishop(color = ChessColor.WHITE)
-        board[getSquaresIndex(Row.ONE, Column.G)] = Bishop(color = ChessColor.WHITE)
-        board[getSquaresIndex(Row.EIGHT, Column.B)] = Bishop(color = ChessColor.BLACK)
-        board[getSquaresIndex(Row.EIGHT, Column.G)] = Bishop(color = ChessColor.BLACK)
+        board[getSquaresIndex(Row.ONE, Column.B)] = Knight(color = ChessColor.WHITE)
+        board[getSquaresIndex(Row.ONE, Column.G)] = Knight(color = ChessColor.WHITE)
+        board[getSquaresIndex(Row.EIGHT, Column.B)] = Knight(color = ChessColor.BLACK)
+        board[getSquaresIndex(Row.EIGHT, Column.G)] = Knight(color = ChessColor.BLACK)
 
-        board[getSquaresIndex(Row.ONE, Column.C)] = Knight(color = ChessColor.WHITE)
-        board[getSquaresIndex(Row.ONE, Column.F)] = Knight(color = ChessColor.WHITE)
-        board[getSquaresIndex(Row.EIGHT, Column.C)] = Knight(color = ChessColor.BLACK)
-        board[getSquaresIndex(Row.EIGHT, Column.F)] = Knight(color = ChessColor.BLACK)
+        board[getSquaresIndex(Row.ONE, Column.C)] = Bishop(color = ChessColor.WHITE)
+        board[getSquaresIndex(Row.ONE, Column.F)] = Bishop(color = ChessColor.WHITE)
+        board[getSquaresIndex(Row.EIGHT, Column.C)] = Bishop(color = ChessColor.BLACK)
+        board[getSquaresIndex(Row.EIGHT, Column.F)] = Bishop(color = ChessColor.BLACK)
 
         board[getSquaresIndex(Row.ONE, Column.D)] = Queen(color = ChessColor.WHITE)
         board[getSquaresIndex(Row.ONE, Column.E)] = King(color = ChessColor.WHITE)
@@ -46,27 +62,103 @@ class ChessBoard() {
         board[getSquaresIndex(Row.EIGHT, Column.E)] = King(color = ChessColor.BLACK)
     }
 
-    fun calculateMovesDestination(piece: ChessPiece, move: Move, distance: Int?): Int? {
-        val currentIndex = getPiecesIndex(piece) ?: return null
-        val potentialDestinationIndex = currentIndex + move.changeOfPositionOnBoard(distance)
+    fun getValidMovesForPiece(piece: ChessPiece): Array<Int> {
+        // get moves that are on the board / not obstructed by your own piece
+        var candidateMoveIndices = getCandidateMoveIndicesForPiece(piece)
 
-        if (potentialDestinationIndex < 0 || potentialDestinationIndex >= ChessBoard.totalSquares) {
-            return null
-        }
-        if (!doesMoveStayOnBoard(move, startingIndex = currentIndex, destinationIndex = potentialDestinationIndex)) {
-            return null
+        // remove moves that check the king
+        for (moveIndex in candidateMoveIndices) {
+            if (doesMovePutKingInCheck(piece, moveIndex)) {
+                candidateMoveIndices.remove(moveIndex)
+            }
         }
 
-        return potentialDestinationIndex
+        return candidateMoveIndices.toTypedArray()
+    }
+
+    fun doesMovePutKingInCheck(piece: ChessPiece, destinationIndex: Int): Boolean {
+        val piecesCurrentIndex = getPiecesIndex(piece) ?: return true
+        val copyOfBoard = board.toList().toTypedArray()
+        copyOfBoard[piecesCurrentIndex] = null
+        copyOfBoard[destinationIndex] = piece
+
+        // get index of the king
+        var enemyKingsPosition: Int = 0
+        for (index in 0..<ChessBoard.totalSquares) {
+            if (copyOfBoard[index] != null && copyOfBoard[index]?.color != piece.color && copyOfBoard[index] is King) {
+                enemyKingsPosition = index
+            }
+        }
+
+        // scan the board for checks from any enemy pieces
+        for (index in 0..<ChessBoard.totalSquares) {
+            if (copyOfBoard[index] != null && copyOfBoard[index]?.color != piece.color) {
+                // found an enemy piece, now see if it can take the king
+                val enemyPiece = copyOfBoard[index] ?: continue
+                val enemyPieceCandidateMoves = getCandidateMoveIndicesForPiece(enemyPiece)
+                if (enemyPieceCandidateMoves.contains(enemyKingsPosition)) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    fun getCandidateMoveIndicesForPiece(piece: ChessPiece): MutableList<Int> {
+        val piecesCurrentIndex = getPiecesIndex(piece) ?: return mutableListOf()
+        var validMoveIndices: MutableList<Int> = arrayListOf()
+        for (move in piece.moveSet) {
+            if (move.isRange()) {
+                for (distance in 1..<ChessBoard.width) {
+                    val destinationIndex: Int = calculateMovesDestinationIndex(piecesCurrentIndex, move, distance) ?: continue
+                    if (!isMovementPathValid(move, piece, destinationIndex)) {
+                        break
+                    }
+                    validMoveIndices.add(destinationIndex)
+                }
+            } else {
+                val destinationIndex: Int = calculateMovesDestinationIndex(piecesCurrentIndex, move, null) ?: continue
+                if (isMovementPathValid(move, piece, destinationIndex)) {
+                    validMoveIndices.add(destinationIndex)
+                }
+            }
+        }
+        return validMoveIndices
+    }
+
+    fun isMovementPathValid(move: Move, piece: ChessPiece, destinationIndex: Int): Boolean {
+        val piecesCurrentIndex = getPiecesIndex(piece) ?: return false
+        // check that the move is on the board
+        if (!doesMoveStayOnBoard(move, startingIndex = piecesCurrentIndex, destinationIndex = destinationIndex)) {
+            return false
+        }
+
+        // check if destination is occupied by your own piece
+        val isSquareOccupied = board[destinationIndex] != null
+        val destinationPiecesColor = board[destinationIndex]?.color
+        if (isSquareOccupied && destinationPiecesColor == piece.color) {
+            return false
+        }
+
+        return true
+    }
+
+    fun calculateMovesDestinationIndex(piecesCurrentIndex: Int, move: Move, distance: Int?): Int? {
+        val destinationIndex = piecesCurrentIndex + move.changeOfPositionOnBoard(distance)
+        return destinationIndex
     }
 
     fun doesMoveStayOnBoard(move: Move, startingIndex: Int, destinationIndex: Int): Boolean {
+        if (destinationIndex < 0 || destinationIndex >= ChessBoard.totalSquares) {
+            return false
+        }
         val startingColumn: Column = getColumn(index = startingIndex) ?: return false
         val destinationColumn: Column = getColumn(index = destinationIndex) ?: return false
         val startingRow: Row = getRow(index = startingIndex) ?: return false
         val destinationRow: Row = getRow(index = destinationIndex) ?: return false
         val columnChange = startingColumn.number - destinationColumn.number
         val rowChange = startingRow.number - destinationRow.number
+
         return when (move) {
             Move.NORTH, Move.SOUTH, Move.RANGENORTH, Move.RANGESOUTH -> startingColumn == destinationColumn
             Move.EAST, Move.WEST, Move.RANGEEAST, Move.RANGEWEST -> startingRow == destinationRow
@@ -146,7 +238,7 @@ enum class Column(val number: Int) {
     E(5), F(6), G(7), H(8);
 
     fun indices(): Array<Int> {
-        var indices: Array<Int> = arrayOf(ChessBoard.height)
+        var indices: Array<Int> = Array(ChessBoard.height) { 0 }
 
         for (index in 0..<ChessBoard.height) {
             indices[index] = (ChessBoard.width * index) + (this.number - 1)
@@ -190,6 +282,14 @@ enum class Move {
             RANGESOUTHWEST -> (-ChessBoard.width - 1) * (distance ?: 0)
         }
     }
+
+    fun isRange(): Boolean {
+        return when (this) {
+            RANGENORTH, RANGESOUTH, RANGEEAST, RANGEWEST,
+            RANGENORTHEAST, RANGENORTHWEST, RANGESOUTHEAST, RANGESOUTHWEST -> true
+            else -> false
+        }
+    }
 }
 
 enum class ChessColor {
@@ -199,6 +299,7 @@ enum class ChessColor {
 sealed class ChessPiece() {
     abstract var moveSet: Array<Move>
     abstract var color: ChessColor
+    override abstract fun toString(): String
 }
 
 class Knight(override var color: ChessColor): ChessPiece() {
@@ -206,6 +307,9 @@ class Knight(override var color: ChessColor): ChessPiece() {
         Move.JUMPNNE, Move.JUMPNNW, Move.JUMPENE, Move.JUMPESE,
         Move.JUMPSSE, Move.JUMPSSW, Move.JUMPWNW, Move.JUMPWSW
     )
+    override fun toString(): String {
+        return "N "
+    }
 }
 
 class Queen(override var color: ChessColor): ChessPiece() {
@@ -213,6 +317,9 @@ class Queen(override var color: ChessColor): ChessPiece() {
         Move.RANGENORTH, Move.RANGESOUTH, Move.RANGEEAST, Move.RANGEWEST,
         Move.RANGENORTHWEST, Move.RANGENORTHEAST, Move.RANGESOUTHWEST, Move.RANGESOUTHEAST
     )
+    override fun toString(): String {
+        return "Q "
+    }
 }
 
 class King(override var color: ChessColor): ChessPiece() {
@@ -220,18 +327,27 @@ class King(override var color: ChessColor): ChessPiece() {
         Move.NORTH, Move.SOUTH, Move.EAST, Move.WEST,
         Move.NORTHWEST, Move.NORTHEAST, Move.SOUTHWEST, Move.SOUTHEAST
     )
+    override fun toString(): String {
+        return "K "
+    }
 }
 
 class Rook(override var color: ChessColor): ChessPiece() {
     override var moveSet: Array<Move> = arrayOf(
         Move.RANGENORTH, Move.RANGESOUTH, Move.RANGEEAST, Move.RANGEWEST
     )
+    override fun toString(): String {
+        return "R "
+    }
 }
 
 class Bishop(override var color: ChessColor): ChessPiece() {
     override var moveSet: Array<Move> = arrayOf(
         Move.RANGENORTHEAST, Move.RANGENORTHWEST, Move.RANGESOUTHEAST, Move.RANGESOUTHWEST
     )
+    override fun toString(): String {
+        return "B "
+    }
 }
 
 sealed class Pawn(override var color: ChessColor): ChessPiece()
@@ -240,11 +356,17 @@ class BlackPawn(): Pawn(ChessColor.BLACK) {
     override var moveSet: Array<Move> = arrayOf(
         Move.SOUTH, Move.SOUTHEAST, Move.SOUTHWEST
     )
+    override fun toString(): String {
+        return "P "
+    }
 }
 
 class WhitePawn(): Pawn(ChessColor.WHITE) {
     override var moveSet: Array<Move> = arrayOf(
         Move.NORTH, Move.NORTHEAST, Move.NORTHWEST
     )
+    override fun toString(): String {
+        return "P "
+    }
 }
 
