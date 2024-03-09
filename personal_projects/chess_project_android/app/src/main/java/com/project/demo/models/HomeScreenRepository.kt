3818f4406a74a27b03918b37fdca05ae53320885
ian rust <ibrust777@gmail.com>
2024-03-09 00:52:14 -0600

    decided on trophy image

--- personal_projects/chess_project_android/app/src/main/java/com/project/demo/models/HomeScreenRepository.kt
+++ personal_projects/chess_project_android/app/src/main/java/com/project/demo/models/HomeScreenRepository.kt
@@ -1,8 +1,8 @@
 package com.project.demo.models
 
-import com.project.demo.R.drawable.trophy_paintnet_transparent_75x75
 import com.project.demo.R.drawable.tape_gold_transparent_75x75
 import com.project.demo.R.drawable.key_75x75_outlined_230alpha
+import com.project.demo.R.drawable.trophy_paintnet_glow2_80x80
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -34,7 +34,7 @@ class HomeScreenRepositoryImpl(
     // TODO: maybe this should really just be an enum, and I can do presentation for these UI-specific values in the viewmodel?
     // or maybe just flat out inject them in the viewmodel (or maybe in a presenter if I want to create one)
     override val playButtonData: ArrayList<HomeScreenRepository.PlayButtonData> = arrayListOf(
-        HomeScreenRepository.PlayButtonData(trophy_paintnet_transparent_75x75, "Play Game"),
+        HomeScreenRepository.PlayButtonData(trophy_paintnet_glow2_80x80, "Play Game"),
         HomeScreenRepository.PlayButtonData(tape_gold_transparent_75x75, "Practice vs AI"),
         HomeScreenRepository.PlayButtonData(key_75x75_outlined_230alpha, "Explore Moves")
     )
