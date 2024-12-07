7cbc501881406b538bb5cd66726ec74a4a0037ec
ian rust <ibrust777@gmail.com>
2024-12-07 16:12:18 -0500

    misc

---
+++ hobbies/cooking/experimental_recipes/couscous/couscous_ingredients.py
@@ -0,0 +1,75 @@
+import random
+
+common_ingredients = [
+    "raisins",
+    "parsley",
+    "lemon juice",
+    "feta",
+    "green onions",
+    "mushrooms",
+    "chicken",
+    "caramelized onions",
+    "zucchini",
+    "red pepper",
+    "sliced green bean",
+    "parmesean",
+    "walnuts",
+    "almonds",
+    "chickpeas",
+    "grated carrot",
+    "mint leaves",
+    "cilantro",
+    "garlic",
+    "cashews",
+    "tomato (cut up)",
+    "spinach",
+    "chives",
+    "gruyere cheese",
+    "egg",
+    "olives",
+    "pine nuts",
+    "dried apricots",
+    "dried cranberries",
+    "pistachios",
+    "pumpkin seeds",
+]
+
+spices = [
+    "smoked paprika",
+    "turmeric",
+    "salt",
+    "chile flakes",
+    "garlic",
+    "cumin",
+    "black pepper",
+    "cinnamon",
+    "corriander",
+    "ginger powder",
+    "powdered cloves (1/8th teaspoon)",
+    "golden saffron",
+    "nutmeg",
+    "cayenne pepper",
+]
+
+print(random.choice(common_ingredients))
+print(random.choice(common_ingredients))
+print(random.choice(common_ingredients))
+print(random.choice(common_ingredients))
+print(random.choice(spices))
+
+
+# go w/ 1.33 cups water per cup of couscous 
+# I've seen some people say 1:1, someone else say 1:1.5, but dice think it's about 1:1.33
+
+# how hot to make water? 
+# 211 - almost exactly boiling 
+
+# how long to let it sit after pouring liquid onto couscous? 
+# 6 minutes apparently... some say 10, I've seen 5 elsewhere... we can try 6. we do have more water...
+
+
+# should we toast the couscous 2-3 minutes? 45/100 - maybe it seems 
+
+# 
+
+
